3ba350828f970eedae68a0c33f3355e3a7b1dd60
清流 <ruoge3s@qq.com>
2020-10-10 12:07:36 +0800

    增加node注册

--- publish/services.php
+++ publish/services.php
@@ -22,18 +22,24 @@ $services = [
     ['UserService', UserServiceInterface::class],
 ];
 
+$nodes = array_filter(array_map(function ($item) {
+    if ($item && $item = explode(':', $item)) {
+        if (count($item) == 2) {
+            return ['host' => $item[0], 'port' => (int)$item[1]];
+        }
+    }
+    return null;
+}, explode(',', env('UNIFY_RPC_NODES', ''))), function ($item) { return $item; });
 
 return [
-    'consumers' => array_map(function ($item){
+    'consumers' => array_map(function ($item) use($nodes) {
         return [
             'name'    => $item[0],
             'service' => $item[1],
             'id'      => $item[1],
             'protocol'=> 'jsonrpc',
             // 需要根据实际情况配置采用consul方式或者直接配置nodes配置服务
-            'nodes' => [
-                 ['host' => '127.0.0.1', 'port' => 9502],
-            ],
+            'nodes' => $nodes,
             'options' => [
                 'connect_timeout'   => 5.0,
                 'recv_timeout'      => 5.0,
